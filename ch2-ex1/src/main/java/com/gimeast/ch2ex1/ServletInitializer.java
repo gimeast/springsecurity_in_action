e9c21517934f75351a84b119930df6b112ef0b81
gimeast94 <dudu.gimeast@gmail.com>
2023-10-30 23:32:12 +0900

    git init

---
+++ ch2-ex1/src/main/java/com/gimeast/ch2ex1/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.gimeast.ch2ex1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Ch2Ex1Application.class);
+    }
+
+}
