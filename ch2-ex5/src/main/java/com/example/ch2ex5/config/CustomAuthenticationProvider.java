ab2ff791d46fba24a11e9685717ceef0ce131bf0
gimeast94 <dudu.gimeast@gmail.com>
2023-11-11 17:58:45 +0900

    CustomAuthenticationProvider에 인증 논리 구현 및 시큐리티 설정에 커스텀한 인증 제공자 등록

--- ch2-ex5/src/main/java/com/example/ch2ex5/config/CustomAuthenticationProvider.java
+++ ch2-ex5/src/main/java/com/example/ch2ex5/config/CustomAuthenticationProvider.java
@@ -1,22 +1,33 @@
 package com.example.ch2ex5.config;
 
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        //인증 논리를 추가할 위치
-        return null;
+
+        String username = authentication.getName();
+        String password = String.valueOf(authentication.getCredentials());
+
+        if("gimeast".equals(username) && "123".equals(password)) {
+            return new UsernamePasswordAuthenticationToken(username, password, Arrays.asList());
+        } else {
+            throw new AuthenticationCredentialsNotFoundException("Error in authentication!");
+        }
     }
 
     @Override
     public boolean supports(Class<?> authentication) {
-        //Authentication 형식의 구현을 추가할 위치
-        return false;
+        return UsernamePasswordAuthenticationToken.class
+                .isAssignableFrom(authentication);
     }
 }
--- ch2-ex5/src/main/java/com/example/ch2ex5/config/ProjectConfig.java
+++ ch2-ex5/src/main/java/com/example/ch2ex5/config/ProjectConfig.java
@@ -1,5 +1,6 @@
 package com.example.ch2ex5.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,5 +12,22 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
+    private final CustomAuthenticationProvider authenticationProvider;
+
+    @Autowired
+    public ProjectConfig(CustomAuthenticationProvider authenticationProvider) {
+        this.authenticationProvider = authenticationProvider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http.authorizeRequests().anyRequest().authenticated();
+    }
 
 }
