9e9bba1d67f498981d0166a1c1e1d55c8a5a84af
gimeast94 <dudu.gimeast@gmail.com>
2023-11-25 01:12:52 +0900

    JdbcUserDetailsManager 이용한 UserDetailsService

---
+++ ch3-ex2/src/main/java/com/example/ch3ex2/Ch3Ex2Application.java
@@ -0,0 +1,13 @@
+package com.example.ch3ex2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch3Ex2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch3Ex2Application.class, args);
+    }
+
+}
